23c12315d44fe5e557575fe0823a1010a1585e87
hexiaojiao <laurel_he@163.com>
2025-03-19 20:16:27 +0800

    一些题目

---
+++ leetcode/238productExceptSelf/productExceptSelf.go
@@ -0,0 +1,13 @@
+package main
+
+// productExceptSelf 返回除自身以外数组值的乘积
+func productExceptSelf(nums []int) []int {
+	/*
+		最快速想到的办法就是全部乘一遍，然后除以自身，但是题目明确要求了不能用除法
+		然后想到直接全部乘一遍，但是题目要求在O(n)复杂度内完成
+	*/
+}
+
+func main() {
+
+}
---
+++ leetcode/238productExceptSelf/productExceptSelf.md
@@ -0,0 +1,3 @@
+除自身以外数组的乘积
+===
+题目来源：[除自身以外数组的乘积](https://leetcode.cn/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=top-interview-150)
